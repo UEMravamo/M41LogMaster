803a00e3a14674076a1e924c28d494e16d825ce5
Christian Jonathan Yang Gonzalez <christianj_yang@icloud.com>
2025-01-10 18:35:42 +0100

    Union de todas las funciones realizadas por el equipo

--- ej1-analisis-log/analisis-log.py
+++ ej1-analisis-log/analisis-log.py
@@ -46,3 +46,43 @@ def obtener_hosts_conectados(df, hostname):
 
     df_result = df_distinct_origin_host.unionByName(df_distinct_destino_host).distinct()
     return [row['host'] for row in df_result.toLocalIterator()]
+
+def filtrar_por_hostname(df, hostname):
+    target_host = hostname.lower()
+    return df.filter(
+        (F.lower(F.col("hostname_origen")) == target_host) |
+        (F.lower(F.col("hostname_destino")) == target_host)
+    )
+
+def obtener_hosts_conectados(df, hostname):
+    target_host = hostname.lower()
+    df_distinct_origin_host = df.filter(
+        F.lower(F.col('hostname_origen')) != target_host
+    ).select(F.col('hostname_origen').alias('host'))
+
+    df_distinct_destino_host = df.filter(
+        F.lower(F.col('hostname_destino')) != target_host
+    ).select(F.col('hostname_destino').alias('host'))
+
+    df_result = df_distinct_origin_host.unionByName(df_distinct_destino_host).distinct()
+    return [row['host'] for row in df_result.toLocalIterator()]
+def analisis_log(input_file, init_datetime, end_datetime, hostname):
+    verificar_archivo(input_file)
+
+    spark = crear_sesion_spark("analisisLog", "4g", "4g", "200")
+    schema = definir_esquema()
+    df_log_csv = cargar_datos_csv(spark, input_file, schema)
+
+    configurar_locale()
+
+    input_format = "%A, %d de %B de %Y %H:%M:%S"
+    init_timestamp, end_timestamp = convertir_fechas(init_datetime, end_datetime, input_format)
+
+    df_filtered = filtrar_datos_por_tiempo(df_log_csv, init_timestamp, end_timestamp)
+    df_filtered = filtrar_por_hostname(df_filtered, hostname)
+
+    hosts = obtener_hosts_conectados(df_filtered, hostname)
+
+    spark.stop()
+
+    return hosts
