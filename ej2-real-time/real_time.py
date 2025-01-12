a93a3a6e26f96c5652d813c7463aac66e6a1cf37
Javier Rafael Cubas Alonso <javiercubas2003@gmail.com>
2025-01-12 20:03:17 +0100

    Correción de bugs

--- ej2-real-time/real_time.py
+++ ej2-real-time/real_time.py
@@ -7,17 +7,21 @@ from watchdog.events import FileSystemEventHandler
 from pyspark.sql import functions as F
 from utils.utils import crear_sesion_spark, definir_esquema, verificar_archivo, cargar_datos_csv
 
+
 class LogFileHandler(FileSystemEventHandler):
-    def __init__(self, input_file, df_log, hostname):
+    def __init__(self, input_file, spark, hostname):
         self.input_file = input_file
-        self.df_log = df_log
+        self.spark = spark
         self.hostname = hostname.lower()
-        self.last_timestamp = int((datetime.now() - timedelta(hours=1)).timestamp() * 1000)
+        self.last_timestamp = int(
+            (datetime.now() - timedelta(hours=1)).timestamp() * 1000)
 
     def process_logs(self):
+        schema = definir_esquema()
+        self.df_log = cargar_datos_csv(self.spark, self.input_file, schema)
 
-        self.df_log.show()
-        df_last_hour = self.df_log.filter(F.col("timestamp") >= self.last_timestamp)
+        df_last_hour = self.df_log.filter(
+            F.col("timestamp") >= self.last_timestamp)
         df_last_hour.show()
         connected_to_host = df_last_hour.filter(
             F.lower(F.col("hostname_destino")) == self.hostname
@@ -41,17 +45,20 @@ class LogFileHandler(FileSystemEventHandler):
         print("Hostname con más conexiones en la última hora:")
         most_connections.show()
 
-        self.last_timestamp = int((datetime.now() - timedelta(hours=1)).timestamp() * 1000)
+        self.last_timestamp = int(
+            (datetime.now() - timedelta(hours=1)).timestamp() * 1000)
 
     def on_modified(self, event):
         if event.src_path == self.input_file:
             print(f"Archivo modificado: {event.src_path}")
             self.process_logs()
 
-def monitor_log_file(input_file, df_log, hostname):
-    event_handler = LogFileHandler(input_file, df_log, hostname)
+
+def monitor_log_file(input_file, spark, hostname):
+    event_handler = LogFileHandler(input_file, spark, hostname)
     observer = Observer()
-    observer.schedule(event_handler, path=os.path.dirname(input_file), recursive=False)
+    observer.schedule(event_handler, path=os.path.dirname(
+        input_file), recursive=False)
     observer.start()
     print(f"Monitorizando cambios en {input_file}...")
     try:
@@ -61,12 +68,11 @@ def monitor_log_file(input_file, df_log, hostname):
         observer.stop()
     observer.join()
 
+
 def real_time(input_file, hostname):
     verificar_archivo(input_file)
 
     spark = crear_sesion_spark("realtime", "4g", "4g", "200")
-    schema = definir_esquema()
-    df_log_csv = cargar_datos_csv(spark, input_file, schema)
-    monitor_log_file(input_file, df_log_csv, hostname)
+    monitor_log_file(input_file, spark, hostname)
 
     spark.stop()
